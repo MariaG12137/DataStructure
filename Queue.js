0e8035cb09893fc0a938625a9ccab1eb6efce2b4
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-08-10 11:33:03 -0400

    Update and rename Queue to Queue.js
    implementation using class

--- Queue.js
+++ Queue.js
@@ -35,7 +35,56 @@ queue.enqueue(4);
 console.log(queue.size()) //1
 console.log(queue.front()); //4
 console.log(queue.isEmpty()) //false
-
+========================================================================================================================================
+  //Creating a queue: enqueue(element), dequeue, front, isEmpty. size
+class Queue 
+{ 
+    // Array is used to implement a Queue 
+    constructor() 
+    { 
+        this.items = []; 
+    } 
+                  
+    // Functions to be implemented 
+    enqueue(element){
+      this.items.push(element);
+    } 
+    dequeue(){
+      if(this.isEmpty()){
+        return false
+      }
+      return this.items.shift()
+    } 
+    front(){
+      if(this.isEmpty()){
+        return false
+      }
+      return this.items[0]
+    } 
+    isEmpty(){
+      if(this.items.length ==0){return true}
+      return false;
+    } 
+    printQueue(){
+      let str = '';
+      for(let item of this.items){
+        str += item;
+      }
+      console.log(str);
+    }
+    size(){
+      return this.items.length;
+    } 
+} 
+let queue = new Queue();
+queue.enqueue(3);
+queue.dequeue();
+queue.enqueue(4);
+console.log(queue.front()); //4
+console.log(queue.isEmpty()) //false
+console.log(queue.size())
+queue.printQueue()
+=========================================================================================================================================
 //The Queue class using ECMAScript 6 syntax
 //Creating a queue: enqueue(element), dequeue, front, isEmpty. size
 let Queue2 = (function(){
@@ -57,7 +106,7 @@ let Queue2 = (function(){
   return Queue2;
   }
   )();
-  
+  ======================================================================================================================================
   //The circular queue - Hot Potato
   function hotPotato(nameList, num){
   let queue = new Queue();
